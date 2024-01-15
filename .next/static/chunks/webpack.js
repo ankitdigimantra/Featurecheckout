d20ae47f296a89d591a7fd6dc02865d9d3c76fd6
dhruvadigi <154401286+dhruvadigi@users.noreply.github.com>
2024-01-15 13:04:06 +0530

    removed unnecessary components

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5b943ec6e3b0cff8"; }
+/******/ 		__webpack_require__.h = function() { return "c33bfa030c4edd0f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
